84760e7dcf51ff127e1cdd0fa785034639e3f943
notcyan <notcyanokay@gmail.com>
2024-10-06 23:07:06 +1000

    Factions
    Created foundation for faction interactions

---
+++ Assets/Scripts/AI/FactionSO.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "EmptyFaction", menuName = "AI/Faction")]
+public class FactionSO : ScriptableObject
+{
+    public string factionName;
+    public FactionSO[] enemyFactions;
+}
